28e07927b239e64800c01e8c65428577c3a990e3
Louistam888 <louistam888@gmail.com>
2024-10-27 18:26:38 -0400

    built basic nav skeleton

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -6,10 +6,31 @@ import { nav } from "framer-motion/client";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const flexBetween = "flex items-center justify-between bg-black";
+  const flexBetween = "flex items-center justify-between";
 
   return (
     <nav>
+      <div className={`${flexBetween} fixed top-Z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          {/* LEFT SIDE */}
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo} />
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Our Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign in</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
