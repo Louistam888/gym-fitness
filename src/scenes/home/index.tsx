fab9ffc6ec4a8a7b7e4404af46003be7e4e4b334
Louistam888 <louistam888@gmail.com>
2024-11-09 15:21:40 -0500

    began laying out home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,12 +1,55 @@
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
-import HomePageText from "@/assets//HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets//SponsorForbes.png";
+import SponsorFortune from "@/assets//SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const Home = ({ setSelectedPage }: Props) => {
-  return <div>Home</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p>
+              Unrivaled gym. Unparalleled training fitness classes. Studios to
+              get the body shapes that you dream of. Get your dream body now.
+            </p>
+          </div>
+          {/* ACTIONS */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
