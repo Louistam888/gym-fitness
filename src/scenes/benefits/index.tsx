e3abf27d1ae88510d6a10484040dd6ed97cb9aa0
Louistam888 <louistam888@gmail.com>
2024-12-12 20:43:58 -0500

    adding individual benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,9 +3,28 @@ import {
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
+import Benefit from "./benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the art facilities",
+    description: "The best of the best",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "Hundreds of diverse classes",
+    description: "More classes than you can count",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and pro trainers",
+    description: "Learn from the best",
+  },
+];
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -17,9 +36,26 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
           <HText>MORE THAN JUST A GYM</HText>
-          <p>copy text</p>
+          <p>
+            We provide world class equipment, trainers and get you to your
+            fitness goals with ease. We put care intoeach and every member.
+          </p>
+        </div>
+
+        {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
         </div>
       </motion.div>
     </section>
